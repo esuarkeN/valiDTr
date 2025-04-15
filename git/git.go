4c28e16e49b5297dcce586b295ae8652202880bb
esuarkeN <nekkrause@gmail.com>
2025-04-15 15:38:42 +0200

    feat: db check

--- git/git.go
+++ git/git.go
@@ -1,8 +1,12 @@
 package git
 
 import (
+	"bytes"
+	"errors"
 	"fmt"
 	"os/exec"
+	"regexp"
+	"strings"
 )
 
 func VerifyCommitSignature(commitHash string) error {
@@ -14,4 +18,22 @@ func VerifyCommitSignature(commitHash string) error {
 
 	fmt.Println(string(output))
 	return nil
+}
+func GetCommitKeyID(commitHash string) (string, error) {
+	cmd := exec.Command("git", "log", "--show-signature", "-1", commitHash)
+	var out bytes.Buffer
+	cmd.Stdout = &out
+	err := cmd.Run()
+	if err != nil {
+		return "", err
+	}
+
+	output := out.String()
+	// Look for "using RSA key <KEYID>"
+	re := regexp.MustCompile(`using \w+ key ([A-F0-9]+)`)
+	match := re.FindStringSubmatch(output)
+	if len(match) < 2 {
+		return "", errors.New("could not extract key ID from signature")
+	}
+	return strings.TrimSpace(match[1]), nil
 }
