73e82019946cdea7e4e4a578d257f987ab777bdd
esuarkeN <nekkrause@gmail.com>
2026-01-13 01:25:29 +0100

    fix: add historical dates

--- cmd/dev_add.go
+++ cmd/dev_add.go
@@ -9,18 +9,38 @@ import (
 )
 
 var devAddName string
+var devAddSince string
+
+func parseDevSince(s string) (time.Time, error) {
+	if s == "" || s == "now" {
+		return time.Now().UTC(), nil
+	}
+	if s == "epoch" {
+		return time.Unix(0, 0).UTC(), nil
+	}
+	t, err := time.Parse(time.RFC3339, s)
+	if err != nil {
+		return time.Time{}, err
+	}
+	return t.UTC(), nil
+}
 
 var devAddCmd = &cobra.Command{
 	Use:   "add-dev [email]",
-	Short: "Add a developer (active starting now)",
+	Short: "Add a developer (active starting at --since)",
 	Args:  cobra.ExactArgs(1),
 	RunE: func(cmd *cobra.Command, args []string) error {
 		email := args[0]
-		return db.AddDeveloper(email, devAddName, time.Now().UTC())
+		since, err := parseDevSince(devAddSince)
+		if err != nil {
+			return err
+		}
+		return db.AddDeveloper(email, devAddName, since)
 	},
 }
 
 func init() {
 	devAddCmd.Flags().StringVar(&devAddName, "name", "", "Optional display name")
+	devAddCmd.Flags().StringVar(&devAddSince, "since", "now", "Start time: now|epoch|RFC3339")
 	rootCmd.AddCommand(devAddCmd)
 }
--- cmd/key_add.go
+++ cmd/key_add.go
@@ -8,17 +8,38 @@ import (
 	"github.com/spf13/cobra"
 )
 
+var keyAddSince string
+
+func parseSince(s string) (time.Time, error) {
+	if s == "" || s == "now" {
+		return time.Now().UTC(), nil
+	}
+	if s == "epoch" {
+		return time.Unix(0, 0).UTC(), nil
+	}
+	t, err := time.Parse(time.RFC3339, s)
+	if err != nil {
+		return time.Time{}, err
+	}
+	return t.UTC(), nil
+}
+
 var keyAddCmd = &cobra.Command{
 	Use:   "add-key [developer-email] [key-id]",
-	Short: "Assign a GPG key to a developer (active starting now)",
+	Short: "Assign a GPG key to a developer (active starting at --since)",
 	Args:  cobra.ExactArgs(2),
 	RunE: func(cmd *cobra.Command, args []string) error {
 		email := args[0]
 		keyID := args[1]
-		return db.AddKeyToDeveloper(email, keyID, time.Now().UTC())
+		since, err := parseSince(keyAddSince)
+		if err != nil {
+			return err
+		}
+		return db.AddKeyToDeveloper(email, keyID, since)
 	},
 }
 
 func init() {
+	keyAddCmd.Flags().StringVar(&keyAddSince, "since", "now", "Start time: now|epoch|RFC3339")
 	rootCmd.AddCommand(keyAddCmd)
 }
